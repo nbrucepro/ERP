263eb10e47c0d5a4bff067835d3d85ceb4f30636
nbrucepro <nezabruce@gmail.com>
2023-05-07 20:59:38 +0200

    setting-up

--- src/main.ts
+++ src/main.ts
@@ -5,13 +5,13 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const config = new DocumentBuilder()
-    .setTitle('ERP docs')
+    .setTitle('ERP docs1')
     .setDescription('ERP documentation')
     .setVersion('1.0')
-    .addTag('Erp')
+    // .addTag('Erp')
     .build();
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('apis', app, document);
+  SwaggerModule.setup('/', app, document);
   await app.listen(4000);
 }
 
